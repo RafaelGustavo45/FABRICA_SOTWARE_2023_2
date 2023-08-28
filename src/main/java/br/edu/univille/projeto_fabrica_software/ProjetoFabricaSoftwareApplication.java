e54eac38901e433d7fd70454ca75bd67ada128db
RAFAEL GUSTAVO REINERT <rafael.reinert@univille.br>
2023-08-28 19:28:52 -0300

    first commit

---
+++ src/main/java/br/edu/univille/projeto_fabrica_software/ProjetoFabricaSoftwareApplication.java
@@ -0,0 +1,13 @@
+package br.edu.univille.projeto_fabrica_software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFabricaSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFabricaSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/univille/projeto_fabrica_software/ProjetoFabricaSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.univille.projeto_fabrica_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFabricaSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
