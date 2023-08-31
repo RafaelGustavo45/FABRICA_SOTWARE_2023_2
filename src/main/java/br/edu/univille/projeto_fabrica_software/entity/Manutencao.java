20e8fda9be6cb73bd664642706d1725d289b1b77
RAFAEL GUSTAVO REINERT <rafael.reinert@univille.br>
2023-08-31 20:29:16 -0300

    aula 03

--- src/main/java/br/edu/univille/projeto_fabrica_software/entity/Manutencao.java
+++ src/main/java/br/edu/univille/projeto_fabrica_software/entity/Manutencao.java
@@ -2,7 +2,34 @@ package br.edu.univille.projeto_fabrica_software.entity;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 public class Manutencao {
-    
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+    private String descricao;
+    @Temporal(TemporalType.DATE)
+    private Date data;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public Date getData() {
+        return data;
+    }
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+
 }
